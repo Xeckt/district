246def8292c9cc10077c77a8e6486e79516f7455
Xeckt <jordansavell1198@gmail.com>
2023-10-10 07:36:50 +0100

    add cleanup func to main, remove error strings and replace with error types

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"log/slog"
 	"os"
@@ -10,16 +11,32 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
+func cleanup(s *discordgo.Session) {
+	Dislog.Info("Cleaning up")
+	go func() {
+		Dislog.Info("Shutting down district session")
+		err := s.Close()
+		if err != nil {
+			Dislog.Error("Error shutting down district", err)
+		}
+		Dislog.Info("Closing log file")
+		err = logFile.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Done. Goodbye!")
+	}()
+}
+
 func main() {
 	if len(Config.Bot.Token) == 0 {
-		Dislog.Info("test?!?!?!?!?")
 		Dislog.Error("Token is empty!")
 		return
 	}
 
 	dg, err := discordgo.New("Bot " + Config.Bot.Token)
 	if err != nil {
-		Dislog.Error(err.Error())
+		Dislog.Error("Could not create session with district", err)
 		return
 	}
 
@@ -33,26 +50,16 @@ func main() {
 
 	err = dg.Open()
 	if err != nil {
-		Dislog.Error(err.Error())
+		Dislog.Error("Error opening district session", err)
 		return
 	}
 
-	// Wait here until CTRL-C or other term signal is received.
-	Dislog.Info("Bot is now running.  Press CTRL-C to exit.")
+	Dislog.Info("Bot is now running. Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
 	<-sc
 
-	Dislog.Info("Shutting down District")
-	err = dg.Close()
-	if err != nil {
-		Dislog.Error(err.Error())
-	}
-	Dislog.Info("Closing log file.")
-	err = logFile.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
+	defer cleanup(dg)
 }
 
 func MemberJoined(s *discordgo.Session, m *discordgo.GuildMemberAdd) {
