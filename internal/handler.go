d5e689a9da9b22293e25464742751147222162c7
Xeckt <jordansavell1198@gmail.com>
2023-10-10 14:29:28 +0100

    create handlermanager via dependency injection - thanks @periaate

---
+++ internal/handler.go
@@ -0,0 +1,23 @@
+package internal
+
+import (
+	"github.com/bwmarrin/discordgo"
+	"log/slog"
+	"reflect"
+	"runtime"
+)
+
+type DistrictHandler interface {
+	AddHandlers(handler ...any)
+}
+
+type HandlerManager struct {
+	*discordgo.Session
+}
+
+func (a HandlerManager) AddHandlers(handlers ...any) {
+	for _, h := range handlers {
+		a.AddHandler(h)
+		Dislog.Info("Handler added", slog.String("Handler", runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()))
+	}
+}
