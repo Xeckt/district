4bd2994142f01326902446645b987d70b07bf179
Xeckt <jordansavell1198@gmail.com>
2023-10-11 09:32:14 +0100

    add intents to string func

--- intents.go
+++ intents.go
@@ -2,10 +2,56 @@ package main
 
 import "github.com/bwmarrin/discordgo"
 
+func IntentString(i discordgo.Intent) string {
+	switch i {
+	case discordgo.IntentsGuilds:
+		return "Guilds"
+	case discordgo.IntentGuildMembers:
+		return "GuildMembers"
+	case discordgo.IntentsGuildBans:
+		return "GuildBans"
+	case discordgo.IntentGuildEmojis:
+		return "GuildEmojis"
+	case discordgo.IntentGuildIntegrations:
+		return "GuildIntegrations"
+	case discordgo.IntentGuildWebhooks:
+		return "GuildWebhooks"
+	case discordgo.IntentGuildInvites:
+		return "GuildInvites"
+	case discordgo.IntentGuildVoiceStates:
+		return "GuildVoiceStates"
+	case discordgo.IntentGuildPresences:
+		return "GuildPresences"
+	case discordgo.IntentGuildMessages:
+		return "GuildMessages"
+	case discordgo.IntentGuildMessageReactions:
+		return "GuildMessageREactions"
+	case discordgo.IntentGuildMessageTyping:
+		return "GuildMessageTyping"
+	case discordgo.IntentDirectMessages:
+		return "DirectMessages"
+	case discordgo.IntentDirectMessageReactions:
+		return "DirectMessageReactions"
+	case discordgo.IntentDirectMessageTyping:
+		return "DirectMessageTyping"
+	case discordgo.IntentMessageContent:
+		return "MessageContent"
+	case discordgo.IntentGuildScheduledEvents:
+		return "GuildScheduledEvents"
+	case discordgo.IntentAutoModerationConfiguration:
+		return "AutoModerationConfiguration"
+	case discordgo.IntentAutoModerationExecution:
+		return "AutoModerationExecution"
+	default:
+		return "No string found for this intent"
+	}
+	return "None found"
+}
+
 func SpecifyIntents(im HandlerManager) {
 	im.AddIntents(
-		discordgo.IntentsGuilds,
-		discordgo.IntentsGuildMembers,
-		discordgo.IntentsGuildMessages,
+		discordgo.IntentGuilds,
+		discordgo.IntentGuildMembers,
+		discordgo.IntentGuildMessages,
 	)
 }
