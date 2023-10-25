99dc36cdba00960532d99946e4be11b59d984198
periaate <daniel.saury.00@gmail.com>
2023-10-25 15:00:28 +0300

    Implemented basic text commands for listing server, user metrics

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 
 require (
 	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/samber/lo v1.38.1 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
--- message.go
+++ message.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"log/slog"
+	"strings"
 
 	"github.com/bwmarrin/discordgo"
 )
 
 func MessageCreated(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if m.Author.ID == s.State.User.ID {
+		return
+	}
+
 	Dislog.Info("Message created", slog.String("message", m.Content))
+	ListUsage(s, m)
+}
+
+func ListUsage(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if strings.HasPrefix(m.Content, "!user") {
+		Dislog.Info("!user command found", slog.String("user_id", m.Author.ID))
+		queryUser(s, m)
+	} else if strings.HasPrefix(m.Content, "!server") {
+		Dislog.Info("!server command found", slog.String("server_id", m.GuildID))
+		queryServer(s, m)
+	} else {
+		// don't update on command
+		Dislog.Info("Updating message count", slog.String("server_id", m.GuildID), slog.String("user_id", m.Author.ID))
+		updateMessageCount(db, m.GuildID, m.Author.ID)
+	}
 }
 
 func AddMessageHandler(dh DistrictHandler) {
